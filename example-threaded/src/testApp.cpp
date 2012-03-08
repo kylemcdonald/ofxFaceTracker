2c734bcb56fb88248198c5ed190004409e336347
Kyle McDonald <kyle@kylemcdonald.net>
2012-03-08 13:28:55 -0500

    regenerated projects and added threaded-example. lots of xcode 4 stuff is making it in. hopefully this will help some people.

---
+++ example-threaded/src/testApp.cpp
@@ -0,0 +1,49 @@
+#include "testApp.h"
+
+using namespace ofxCv;
+
+void testApp::setup() {
+	ofSetVerticalSync(true);
+	ofSetDrawBitmapMode(OF_BITMAPMODE_MODEL_BILLBOARD);
+	
+	cam.initGrabber(640, 480);
+	
+	tracker.setup();
+}
+
+void testApp::update() {
+	cam.update();
+	if(cam.isFrameNew()) {
+		tracker.update(toCv(cam));
+		position = tracker.getPosition();
+		scale = tracker.getScale();
+		orientation = tracker.getOrientation();
+		rotationMatrix = tracker.getRotationMatrix();
+	}
+}
+
+void testApp::draw() {
+	ofSetColor(255);
+	cam.draw(0, 0);
+	ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
+	
+	if(tracker.getFound()) {
+		ofSetLineWidth(1);
+		tracker.draw();
+		
+		//easyCam.begin();
+		ofSetupScreenOrtho(640, 480, OF_ORIENTATION_UNKNOWN, true, -1000, 1000);
+		ofTranslate(640 / 2, 480 / 2);
+		applyMatrix(rotationMatrix);
+		ofScale(5,5,5);
+		ofDrawAxis(scale);
+		tracker.getObjectMesh().drawWireframe();
+		//easyCam.end();
+	}
+}
+
+void testApp::keyPressed(int key) {
+	if(key == 'r') {
+		tracker.reset();
+	}
+}
---
+++ example-threaded/src/testApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+using namespace ofxCv;
+using namespace cv;
+
+#include "ofxFaceTrackerThreaded.h"
+
+class testApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	
+	ofVideoGrabber cam;
+	ofxFaceTrackerThreaded tracker;
+	ofVec2f position;
+	float scale;
+	ofVec3f orientation;
+	ofMatrix4x4 rotationMatrix;
+	
+	Mat translation, rotation;
+	ofMatrix4x4 pose;
+	
+	ofEasyCam easyCam;
+};
