df97ee9167cf1075de65956d4e9d09eb3706b880
Kyle McDonald <kyle@kylemcdonald.net>
2014-06-17 18:23:15 -0400

    added example of aligning images to eyes and exporting the results

---
+++ example-align-eyes/src/main.cpp
@@ -0,0 +1,8 @@
+#include "testApp.h"
+#include "ofAppGlutWindow.h"
+
+int main() {
+	ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 128, 128, OF_WINDOW);
+	ofRunApp(new testApp());
+}
---
+++ example-align-eyes/src/testApp.cpp
@@ -0,0 +1,55 @@
+#include "testApp.h"
+
+using namespace ofxCv;
+
+void testApp::setup() {
+	tracker.setup();
+    dir.listDir("raw");
+    cur = 0;
+    eyeDistance = 32;
+    fbo.allocate(ofGetWidth(), ofGetHeight());
+}
+
+void testApp::update() {
+    string filename = dir.getFile(cur).getFileName();
+    img.loadImage("raw/" + filename);
+    tracker.reset();
+    if(tracker.update(toCv(img))) {
+        fbo.begin();
+        ofClear(255, 255);
+        ofPushMatrix();
+        ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+        ofVec2f leftEye = tracker.getImageFeature(ofxFaceTracker::LEFT_EYE).getCentroid2D();
+        ofVec2f rightEye = tracker.getImageFeature(ofxFaceTracker::RIGHT_EYE).getCentroid2D();
+        float curDistance = leftEye.distance(rightEye);
+        float scale = eyeDistance / curDistance;
+        float rotation = (rightEye - leftEye).angle(ofVec2f(1, 0));
+        ofTranslate(-eyeDistance / 2, 0);
+        ofScale(scale, scale);
+        ofRotate(rotation);
+        ofTranslate(-leftEye.x, -leftEye.y);
+        img.draw(0, 0);
+        ofPopMatrix();
+        fbo.end();
+        fbo.readToPixels(pix);
+        pix.setImageType(OF_IMAGE_GRAYSCALE);
+        ofSaveImage(pix, "aligned/" + ofToString(tracker.getOrientation().y) + "_" + filename + ".tiff");
+    }
+    cur++;
+    if(cur == dir.size()) {
+        cur = 0;
+        ofLog() << "done";
+        ofExit();
+    }
+}
+
+void testApp::draw() {
+    fbo.draw(0, 0);
+	ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
+}
+
+void testApp::keyPressed(int key) {
+	if(key == 'r') {
+		tracker.reset();
+	}
+}
