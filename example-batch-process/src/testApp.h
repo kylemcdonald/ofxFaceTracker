dfbb9cfbd7ea58cc5e77e90d74b0cf2e97baa307
Kyle McDonald <kyle@kylemcdonald.net>
2015-03-28 02:02:28 +0100

    batch process all faces in folder and save meshes to disk

---
+++ example-batch-process/src/testApp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+
+#include "ofxFaceTracker.h"
+#include "ofxTiming.h"
+
+class testApp : public ofBaseApp {
+public:
+    void setup();
+    void update();
+	void draw();
+    void saveFace(), loadFace();
+    
+    ofDirectory dir;
+    int index;
+	ofxFaceTracker tracker;
+    int iterations;
+    ofImage img;
+    ofMesh prevFace;
+    RateTimer timer;
+};
