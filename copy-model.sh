d76e92d99252341762022583c9ae7a38169d0a44
Kyle McDonald <kyle@kylemcdonald.net>
2015-11-08 15:29:46 -0500

    removed project files

---
+++ copy-model.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import glob, re, shutil, fileinput, os
+
+sourceProjectName = "EmptyExample"
+sourceModelPath = "libs/FaceTracker/model/"
+
+examples = glob.glob("example*")
+for example in examples:
+    # model data
+    targetModelPath = example + "/bin/data/model/"
+    try: shutil.rmtree(targetModelPath)
+    except: pass
+    shutil.copytree(sourceModelPath, targetModelPath)
+
+    print "Copied model data into " + targetModelPath
